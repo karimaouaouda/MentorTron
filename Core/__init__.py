b5ed39d84760a37d3b9bddd90f2262acbf6a9c65
Karim Aouaouda <karimaouaouda.officiel@gmail.com>
2024-10-04 18:48:59 +0200

    setup the project

---
+++ Core/__init__.py
@@ -0,0 +1,13 @@
+"""
+every embded system has two main functions, steup() and run()
+setup() used to initialize the default values and variables for the system
+run() is the main loop for the system
+"""
+
+def setup():
+    pass
+
+
+def run():
+
+    pass
